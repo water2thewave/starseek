aeeb7189c6e4704ea4089d2c8450a5700079d334
Kirishima <97999610+water2thewave@users.noreply.github.com>
2022-03-19 02:00:01 -0400

    telescope routing

---
+++ src/routes/telescope/[cid].svelte
@@ -0,0 +1,17 @@
+<script>
+  
+  import ContestellationHome from "$lib/components/ConstellationHome.svelte";
+  import RoleSelector from "$lib/components/RoleSelector.svelte";
+  import { writable } from '$lib/browserStore.js';
+  // import * as rolesJson from "../../../../static/roles.json";
+
+  import { page, session } from "$app/stores";
+  import { onMount } from "svelte";
+
+  console.log('page params', $page.params);
+</script>
+
+
+telescope here {$page.params.cid}
+
+<!-- </ContestellationHome> -->
